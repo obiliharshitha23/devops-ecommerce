562f74842d6e7b4909b96d4c8ab2b46a7e61326d
obiliharshitha23 <124793355+obiliharshitha23@users.noreply.github.com>
2026-08-05 22:14:39 +0530

    Update ProductController.java

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -1,5 +1,7 @@
-package com.ecommerce.controller;
+### ProductController.java
 
+```java
+package com.ecommerce.controller;
 
 import com.ecommerce.entity.Product;
 import com.ecommerce.service.ProductService;
@@ -8,68 +10,188 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
-
 @RequestMapping("/api/products")
-
 public class ProductController {
 
 
-
-private final ProductService service;
-
-
-public ProductController(ProductService service){
-
-this.service=service;
-
-}
+    private final ProductService productService;
 
 
+    public ProductController(ProductService productService) {
 
-@PostMapping
+        this.productService = productService;
 
-public Product addProduct(
-@RequestBody Product product){
-
-return service.save(product);
-
-}
+    }
 
 
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
 
-@GetMapping
+        return productService.save(product);
 
-public List<Product> getProducts(){
-
-return service.getAll();
-
-}
+    }
 
 
+    @GetMapping
+    public List<Product> getAllProducts() {
 
-@GetMapping("/{id}")
+        return productService.getAll();
 
-public Product getProduct(
-@PathVariable Long id){
-
-return service.getById(id);
-
-}
+    }
 
 
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
 
-@DeleteMapping("/{id}")
+        return productService.getById(id);
 
-public String delete(
-@PathVariable Long id){
+    }
 
-service.delete(id);
 
-return "Deleted";
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+
+        productService.delete(id);
+
+        return "Product deleted successfully";
+
+    }
 
 }
+```
 
+---
+
+### UserController.java
+
+```java
+package com.ecommerce.controller;
+
+
+import com.ecommerce.entity.User;
+import com.ecommerce.service.UserService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+
+        this.userService = userService;
+
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+
+        return userService.save(user);
+
+    }
+
+
+    @GetMapping
+    public List<User> getAllUsers() {
+
+        return userService.getAllUsers();
+
+    }
+
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+
+        return userService.getUserById(id);
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+
+        userService.deleteUser(id);
+
+        return "User deleted successfully";
+
+    }
 
 }
+```
+
+---
+
+### OrderController.java
+
+```java
+package com.ecommerce.controller;
+
+
+import com.ecommerce.entity.Order;
+import com.ecommerce.service.OrderService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+
+    private final OrderService orderService;
+
+
+    public OrderController(OrderService orderService) {
+
+        this.orderService = orderService;
+
+    }
+
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+
+        return orderService.createOrder(order);
+
+    }
+
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+
+        return orderService.getAllOrders();
+
+    }
+
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable Long id) {
+
+        return orderService.getOrderById(id);
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deleteOrder(@PathVariable Long id) {
+
+        orderService.deleteOrder(id);
+
+        return "Order deleted successfully";
+
+    }
+
+}
+```
+
