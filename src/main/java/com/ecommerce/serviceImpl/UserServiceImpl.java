c641734a98311c98fbaca583e55cfb41dcdcc4ab
obiliharshitha23 <124793355+obiliharshitha23@users.noreply.github.com>
2026-08-05 21:15:50 +0530

    Update UserServiceImpl.java

--- src/main/java/com/ecommerce/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/ecommerce/serviceImpl/UserServiceImpl.java
@@ -1,1 +1,65 @@
+### UserServiceImpl.java
+
+```java
+package com.ecommerce.serviceImpl;
+
+import com.ecommerce.entity.User;
+import com.ecommerce.repository.UserRepository;
+import com.ecommerce.service.UserService;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    private final UserRepository userRepository;
+
+
+    public UserServiceImpl(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+
+    }
+
+
+    @Override
+    public User save(User user) {
+
+        return userRepository.save(user);
+
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+
+        return userRepository.findAll();
+
+    }
+
+
+    @Override
+    public User getUserById(Long id) {
+
+        return userRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+
+    }
+
+
+    @Override
+    public void deleteUser(Long id) {
+
+        userRepository.deleteById(id);
+
+    }
+
+
+}
+```
 
