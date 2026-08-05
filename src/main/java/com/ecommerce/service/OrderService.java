66cb29cc79557661c68e41f97a4471ba2b25e277
obiliharshitha23 <124793355+obiliharshitha23@users.noreply.github.com>
2026-08-05 21:20:39 +0530

    Update OrderService.java

--- src/main/java/com/ecommerce/service/OrderService.java
+++ src/main/java/com/ecommerce/service/OrderService.java
@@ -1,1 +1,9 @@
-
+package com.ecommerce.service; 
+import com.ecommerce.entity.Order;
+import java.util.List; 
+public interface OrderService { 
+  Order createOrder(Order order); 
+  List<Order> getAllOrders();
+  Order getOrderById(Long id); 
+  void deleteOrder(Long id); 
+}
