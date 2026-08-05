4ca68e1e3445d5ca843062e7f721c2843e6b87e0
obiliharshitha23 <124793355+obiliharshitha23@users.noreply.github.com>
2026-08-05 22:16:39 +0530

    Update UserController.java

--- src/main/java/com/ecommerce/controller/UserController.java
+++ src/main/java/com/ecommerce/controller/UserController.java
@@ -1,1 +1,64 @@
+### UserController.java
 
+```java
+package com.ecommerce.controller;
+
+
+import com.ecommerce.entity.User;
+import com.ecommerce.service.UserService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+
+        this.userService = userService;
+
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+
+        return userService.save(user);
+
+    }
+
+
+    @GetMapping
+    public List<User> getAllUsers() {
+
+        return userService.getAllUsers();
+
+    }
+
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+
+        return userService.getUserById(id);
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+
+        userService.deleteUser(id);
+
+        return "User deleted successfully";
+
+    }
+
+}
+```
