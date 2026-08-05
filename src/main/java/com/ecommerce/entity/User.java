e189d9adad47388cc092ac8baa25f4eca9f0ef7d
obiliharshitha23 <124793355+obiliharshitha23@users.noreply.github.com>
2026-08-05 21:06:21 +0530

    Create User.java

---
+++ src/main/java/com/ecommerce/entity/User.java
@@ -0,0 +1,33 @@
+package com.ecommerce.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name="users")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+
+    private String email;
+
+
+    private String password;
+
+
+}
