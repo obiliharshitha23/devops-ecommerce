02de10eae498972c239439fc6c191b6d53bf4a10
obiliharshitha23 <124793355+obiliharshitha23@users.noreply.github.com>
2026-08-05 21:19:37 +0530

    Update UserService.java

--- src/main/java/com/ecommerce/service/UserService.java
+++ src/main/java/com/ecommerce/service/UserService.java
@@ -1,1 +1,9 @@
-
+package com.ecommerce.service; 
+import com.ecommerce.entity.User; 
+import java.util.List; 
+public interface UserService { 
+  User save(User user); 
+  List<User> getAllUsers(); 
+  User getUserById(Long id); 
+  void deleteUser(Long id); 
+}
